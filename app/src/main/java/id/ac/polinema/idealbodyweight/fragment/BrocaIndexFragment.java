ad6a3a349e24c5a2fc37fba302cbe7d9ab0cdef5
jimmyaudrio <jimmyaudrio16111999@gmail.com>
2019-10-14 21:37:55 +0700

    Praktikum selesai

--- app/src/main/java/id/ac/polinema/idealbodyweight/fragment/BrocaIndexFragment.java
+++ app/src/main/java/id/ac/polinema/idealbodyweight/fragment/BrocaIndexFragment.java
@@ -98,3 +98,6 @@ public class BrocaIndexFragment extends Fragment {
 
     }
 }
+
+
+//selesai
--- app/src/main/java/id/ac/polinema/idealbodyweight/fragment/MenuFragment.java
+++ app/src/main/java/id/ac/polinema/idealbodyweight/fragment/MenuFragment.java
@@ -92,3 +92,5 @@ public class MenuFragment extends Fragment {
         void onBodyMassIndexButtonClicked();
     }
 }
+
+//selesai
